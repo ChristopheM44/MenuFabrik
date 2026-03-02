344e97e876d3f082bb68fb2fcf587fc765294e93
Christophe <vilmur@orange.fr>
2026-03-02 17:38:18 +0100

    Fix darktheme

--- MenuFabrikWeb/src/main.ts
+++ MenuFabrikWeb/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
+import { definePreset } from '@primeuix/themes';
 import ConfirmationService from 'primevue/confirmationservice';
 import ToastService from 'primevue/toastservice';
 import './style.css'
@@ -11,11 +12,26 @@ import router from './router'
 
 const app = createApp(App)
 
+const LighterDarkPreset = definePreset(Aura, {
+    semantic: {
+        colorScheme: {
+            dark: {
+                surface: {
+                    950: '#111827',
+                    900: '#1f2937',
+                    800: '#374151',
+                    700: '#4b5563',
+                }
+            }
+        }
+    }
+});
+
 app.use(createPinia())
 app.use(router)
 app.use(PrimeVue, {
     theme: {
-        preset: Aura,
+        preset: LighterDarkPreset,
         options: {
             darkModeSelector: '.dark',
         }
