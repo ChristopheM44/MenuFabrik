720e22d5a423c06474596511ff2c5bbbf6606753
Christophe <vilmur@orange.fr>
2026-03-22 21:00:32 +0100

    Fix list recipes

--- MenuFabrikWeb/src/components/recipes/RecipeGrid.vue
+++ MenuFabrikWeb/src/components/recipes/RecipeGrid.vue
@@ -20,7 +20,7 @@ const emit = defineEmits<{
 }>();
 
 const gridClass = computed(() =>
-    props.gridCols ?? 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'
+    props.gridCols ?? 'grid-cols-1 md:grid-cols-2'
 );
 
 const formatTime = (minutes: number) => {
